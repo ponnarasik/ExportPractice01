da9d5d0fac598c97bff12a5f3d146d9bd8835862
ponnarasi <85145307+ponnarasik@users.noreply.github.com>
2021-06-24 23:30:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const palindromeCheck = require('./practiceExports.js');
+
+console.log(typeof palindromeCheck);
+console.log(palindromeCheck('that'));
+console.log(palindromeCheck('radar'));
+
+
+
+function isPalindrome(str){
+   return str === str.split('').reverse().join('');
+}
+
+function evenOrOdd(num){
+   if (num%2===0){
+      return "Even";
+   } else {
+      return "Odd";
+   }
+}
+
+function randomArrayElement(arr){
+   let index = Math.floor(Math.random()*arr.length);
+   return arr[index];
+}
+
